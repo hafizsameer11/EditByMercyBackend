01cde79993f96edba3396535ddc613b54e6b0a62
hafiz sameer <hmstech11@gmail.com>
2025-07-26 20:27:37 +0500

    hel

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\FeedController;
 use App\Http\Controllers\Api\QuickReplyController;
 use App\Http\Controllers\QuestionareController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
@@ -15,7 +16,27 @@ Route::get('/login', function () {
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/optimize-app', function () {
+    Artisan::call('optimize:clear'); // Clears cache, config, route, and view caches
+    Artisan::call('cache:clear');    // Clears application cache
+    Artisan::call('config:clear');   // Clears configuration cache
+    Artisan::call('route:clear');    // Clears route cache
+    Artisan::call('view:clear');     // Clears compiled Blade views
+    Artisan::call('config:cache');   // Rebuilds configuration cache
+    Artisan::call('route:cache');    // Rebuilds route cache
+    Artisan::call('view:cache');     // Precompiles Blade templates
+    Artisan::call('optimize');       // Optimizes class loading
 
+    return "Application optimized and caches cleared successfully!";
+});
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    return response()->json(['message' => 'Migration successful'], 200);
+});
+Route::get('/migrate/rollback', function () {
+    Artisan::call('migrate:rollback');
+    return response()->json(['message' => 'Migration rollback successfully'], 200);
+});
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
