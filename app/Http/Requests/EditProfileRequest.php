bf16cbfa9581832227a0f20d09fe2a64337f4cec
hafiz sameer <hmstech11@gmail.com>
2025-07-27 22:48:30 +0500

    hel

---
+++ app/Http/Requests/EditProfileRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class EditProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['nullable'],
+            'email' => ['nullable'],
+            'password' => ['nullable'],
+            'role' => ['nullable', 'in:admin,support,editor,chief_editor,user'],
+            'profile_picture' => 'nullable',
+            'phone'=>'nullable'
+        ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        // Throw a JSON response when validation fails
+        throw new HttpResponseException(
+            response()->json([
+                'status' => 'error',
+                'data' => $validator->errors(),
+                'message' => $validator->errors()->first()
+            ], 422)
+        );
+    }
+}
