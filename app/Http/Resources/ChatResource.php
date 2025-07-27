e0e552b650afe2149d79eb99ea1c51806e8b1cb4
hafiz sameer <hmstech11@gmail.com>
2025-07-27 23:49:31 +0500

    hel

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class ChatResource extends JsonResource
 {
@@ -14,19 +15,29 @@ class ChatResource extends JsonResource
      */
   public function toArray($request)
     {
+        $currentUserId = Auth::id();
+
+        $isUserA = $this->user_id === $currentUserId;
+
         return [
             'id'           => $this->id ?? null,
             'type'         => $this->type ?? 'chat',
             'user_id'      => $this->user_id ?? null,
             'user_2_id'    => $this->user_2_id ?? null,
             'agent_id'     => $this->agent_id ?? null,
-            'created_at'   => $this->created_at?? null,
+            'created_at'   => $this->created_at ?? null,
             'updated_at'   => $this->updated_at ?? null,
 
             'messages'     => MessageResource::collection($this->whenLoaded('messages')),
 
-            'participant_a'=> new UserMiniResource($this->whenLoaded('participantA')),
-            'participant_b'=> new UserMiniResource($this->whenLoaded('participantB')),
+            'participant_a'=> $isUserA 
+                                ? new UserMiniResource($this->whenLoaded('participantA'))
+                                : new UserMiniResource($this->whenLoaded('participantB')),
+
+            'participant_b'=> $isUserA 
+                                ? new UserMiniResource($this->whenLoaded('participantB'))
+                                : new UserMiniResource($this->whenLoaded('participantA')),
+
             'agent'        => new UserMiniResource($this->whenLoaded('agent')),
         ];
     }
