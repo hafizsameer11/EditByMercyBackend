e2cea9d372a977f65e4d8626f971eb549679d37f
hafiz sameer <hmstech11@gmail.com>
2025-07-27 18:14:12 +0500

    hel

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -13,7 +13,7 @@ class OrderController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $orders = Order::where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
+        $orders = Order::with('agent')->where('user_id', $user->id)->orderBy('created_at', 'desc')->get();
         return ResponseHelper::success($orders, "Orders fetched successfully.");
         // $orders=Order::;
     }
