ce229845e896f47b0afd261c15059dc11bac3aed
hafiz sameer <hmstech11@gmail.com>
2025-08-19 02:36:41 +0500

    hel

--- app/Repositories/ChatRepository.php
+++ app/Repositories/ChatRepository.php
@@ -25,10 +25,21 @@ class ChatRepository
         return null;
     }
     // public function 
-    public function getChatByUserId(int $userId)
-    {
-        return Chat::where('user_id', $userId)->orWhere('user_2_id', $userId)->with('messages', 'participantA', 'participantB', 'agent','order')->orderBy('created_at', 'desc')->get();
-    }
+  public function getChatByUserId(int $userId)
+{
+    return Chat::where('user_id', $userId)
+        ->orWhere('user_2_id', $userId)
+        ->with(['messages', 'participantA', 'participantB', 'agent', 'order'])
+        ->withCount([
+            'messages as unread_count' => function ($query) use ($userId) {
+                $query->where('is_read', false)
+                      ->where('sender_id', '!=', $userId); // exclude user's own messages
+            }
+        ])
+        ->orderBy('created_at', 'desc')
+        ->get();
+}
+
     public function getChatByUserIdSIngle(int $userId)
     {
         return Chat::where('user_id', $userId)->orWhere('user_2_id', $userId)->with('messages', 'participantA', 'participantB', 'agent')->orderBy('created_at', 'desc')->first();
